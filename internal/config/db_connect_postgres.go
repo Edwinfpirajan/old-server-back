b86fef191240db229ab228f99ffa5030b158816f
Edwinfpirajan <efpa1998@hotmail.com>
2023-09-01 09:07:59 -0500

    update packaje.json

--- internal/config/db_connect_postgres.go
+++ internal/config/db_connect_postgres.go
@@ -15,10 +15,12 @@ var DB *gorm.DB
 func ConnectDB() {
 	loadEnv()
 
-	dsn := buildDSN()
-	// dsn := "user=asistencia password=1234456 dbname=asistencia port=5432 sslmode=disable"
+	// dsn := buildDSN()
+	dsn := "host=containers-us-west-210.railway.app user=postgres password=7xt3Vx6eAevhZTMmSiGJ dbname=railway port=7112 sslmode=disable"
 	// PGPASSWORD=7xt3Vx6eAevhZTMmSiGJ psql -h containers-us-west-210.railway.app -U postgres -p 7112 -d railway
 	// dsn := "host=localhost user=asistencias password=123456 dbname=asistencias port=5432 sslmode=disable"
+
+	// PGPASSWORD=7xt3Vx6eAevhZTMmSiGJ psql -h containers-us-west-210.railway.app -U postgres -p 7112 -d railway
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
